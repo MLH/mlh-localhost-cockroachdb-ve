2632c1cb59f58e35c05585f98ae49685439b0224
Jamie <jamie@mlh.io>
2018-01-16 12:41:18 -0500

    add source code

---
+++ server.js
@@ -0,0 +1,20 @@
+// Load Express and Database Libraries
+var express = require('express')
+var db = require('./db')
+
+// Create a new Express application
+var app = express()
+
+// Serve files in the /public directory
+app.use(express.static('public'))
+
+// Call this function whenever someone requests the /quote path
+app.get('/quote', function (req, res) {
+  // Get a random quote and pass it to the browser.
+  db.query('SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1')
+    .then(data => res.send(data.rows[0]))
+    .catch(err => res.send({ error: "BRB! Connecting to CockroachDB" }))
+})
+
+// Start the server on port 8081
+app.listen(8081, () => console.log("Listening on http://localhost:8081/"));
